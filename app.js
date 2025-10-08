d91d160b00bfb914525b565f1f450c046726bd9c
FelixR77 <felix.romero75@gmail.com>
2025-10-08 14:03:06 -0500

    some variables cached and event listners

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
-const startButton = querySelector('.start-button');
+const startButton = querySelector(".start-button");
 
-const timer = querySelector('.timer');
+const timer = querySelector('#time-tracker');
 
-const score = querySelector('score');
+const score = querySelector('#score-tracker');
 
 const allButtons = querySelectorAll('#game-container');
 
+let timeInSeconds = 60; 
 
+function formatTime(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+}
+
+function gameStart {
+    timer = 0; 
+    score = 0; 
+    
+    
+}
+
+
+startButton.addEventListener(click, ()) 
