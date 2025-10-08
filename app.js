293502331b20e34b62a1833e887534beacfe8d20
FelixR77 <felix.romero75@gmail.com>
2025-10-08 11:52:57 -0500

    Going into working on the start button

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log('hello world!')
+const startButton = querySelector('.start-button');
+
+const timer = querySelector('.timer');
+
+const score = querySelector('score');
+
+const allButtons = querySelectorAll('#game-container');
+
+
