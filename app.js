6f9a44d81049c8743bf5bb4644fd0c9eeb7c8cb2
FelixR77 <felix.romero75@gmail.com>
2025-10-09 09:26:25 -0500

    format of time done - moving on to set up start button

--- app.js
+++ app.js
@@ -1,24 +1,27 @@
-const startButton = querySelector(".start-button");
+const startButton = document.querySelector(".start-button");
 
-const timer = querySelector('#time-tracker');
+const timer = document.querySelector('#time-tracker');
 
-const score = querySelector('#score-tracker');
+const score = document.querySelector('#score-tracker');
 
-const allButtons = querySelectorAll('#game-container');
+const allButtons = document.querySelectorAll('#game-container');
 
 let timeInSeconds = 60; 
 
 function formatTime(seconds) {
     const minutes = Math.floor(seconds / 60);
     const remainingSeconds = seconds % 60;
+    return `${minutes}:${remainingSeconds}`
 }
+console.log(formatTime(90))
+//  Learn more about how this function works ^^
 
-function gameStart {
-    timer = 0; 
-    score = 0; 
+// function gameStart {
+//     timer = 90; 
+//     score = 0; 
     
     
-}
+// }
 
 
-startButton.addEventListener(click, ()) 
+// startButton.addEventListener(click, ()) 
